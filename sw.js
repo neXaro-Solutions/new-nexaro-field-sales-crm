3f7df4eb7ee6fbd7321f5ee1857b04c440fbc3b2
neXaro-Solutions <kontakt@nexaro-solutions.de>
2026-09-11 10:45:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='nexaro-field-crm-v3-9';
-const ASSETS=['./','./index.html','./styles.css','./app.js?v=390','./manifest.webmanifest','./icon.svg'];
+const CACHE='nexaro-field-crm-v3-10';
+const ASSETS=['./','./index.html','./styles.css','./app.js?v=391','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
