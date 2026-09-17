049e16cb30339e3c8724329e1bf8b35add602128
neXaro-Solutions <kontakt@nexaro-solutions.de>
2026-09-17 12:54:50 +0200

    Refresh area search assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nexaro-field-crm-v3-16';
+const CACHE='nexaro-field-crm-v3-17';
 const ASSETS=['./index.html','./styles.css?v=380','./app.js?v=370','./area-fix.js?v=2','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
