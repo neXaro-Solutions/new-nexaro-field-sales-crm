f2c7c84988296cb15a456e92432ef0c49f1670be
neXaro-Solutions <kontakt@nexaro-solutions.de>
2026-09-17 17:27:43 +0200

    Start clean SumUp advisor v2 foundation

---
+++ sumup-advisor-clean-v2.js
@@ -0,0 +1,11 @@
+(()=>{
+'use strict';
+// neXaro Solutions - clean SumUp advisor foundation
+// New UI layer: Kunde -> Bedarf -> Empfehlung
+window.neXaroSumUpCleanV2={
+ version:'2.0',
+ status:'clean-berater-ui-start',
+ steps:['customer','requirements','recommendation'],
+ hideLegacyComparison:true
+};
+})();
